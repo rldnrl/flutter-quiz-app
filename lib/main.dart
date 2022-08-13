33826be1d62baecb38c9d01f06384d8538fa5840
rldnrl <devpark1002@gmail.com>
2022-08-13 20:20:10 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First Flutter'),
+        ),
+        body: const Text('This is my default text'),
+      ),
+    );
+  }
+}
