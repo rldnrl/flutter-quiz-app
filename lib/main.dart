7718bf16d4521f3f0f33672b8900bcb37753de8f
rldnrl <devpark1002@gmail.com>
2022-08-14 19:56:23 +0900

    feat: add layout widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,32 @@ void main(List<String> args) {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First Flutter'),
         ),
-        body: const Text('This is my default text'),
+        body: Column(
+          children: const [
+            Text('The Question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer3'),
+            ),
+          ],
+        ),
       ),
     );
   }
