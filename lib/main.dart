7c365a49773a0ad85d21818e45dba088f5cccee9
rldnrl <devpark1002@gmail.com>
2022-08-14 20:11:39 +0900

    feat: binding onPressed handler

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,18 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion1() {
+    print('Answer1 choosen');
+  }
+
+  void answerQuestion2() {
+    print('Answer2 choosen');
+  }
+
+  void answerQuestion3() {
+    print('Answer3 choosen');
+  }
+
   @override
   Widget build(BuildContext context) {
     var questions = [
@@ -17,19 +29,19 @@ class MyApp extends StatelessWidget {
           title: const Text('My First Flutter'),
         ),
         body: Column(
-          children: const [
-            Text('The Question!'),
+          children: [
+            const Text('The Question!'),
             ElevatedButton(
-              onPressed: null,
-              child: Text('Answer1'),
+              onPressed: answerQuestion1,
+              child: const Text('Answer1'),
             ),
             ElevatedButton(
-              onPressed: null,
-              child: Text('Answer2'),
+              onPressed: answerQuestion2,
+              child: const Text('Answer2'),
             ),
             ElevatedButton(
-              onPressed: null,
-              child: Text('Answer3'),
+              onPressed: answerQuestion3,
+              child: const Text('Answer3'),
             ),
           ],
         ),
